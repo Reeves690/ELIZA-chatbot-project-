263fc67de33127aa70c7f6ea394b9598786c5968
Reeves690 <reevesholliday@my.unt.edu>
2026-07-21 14:05:25 -0500

    Fix regex patterns for financial keywords

--- Financial.cpp
+++ Financial.cpp
@@ -20,13 +20,13 @@ namespace {
         { std::regex(R"(\bbankrupt(cy)?\b)", std::regex::icase),
           { "That sounds like a very stressful situation. What's been happening financially?" } },
 
-        { std::regex(R"(\b(debt|owe|owing) (.*)$)", std::regex::icase),
+        { std::regex(R"(\b(debt|owe|owing)\b.*$)", std::regex::icase),
           { "How is dealing with %R% affecting you?" } },
 
         { std::regex(R"(\bi (can't afford|cannot afford) (.*)$)", std::regex::icase),
           { "Not being able to afford %R% -- how does that weigh on you?" } },
 
-        { std::regex(R"(\b(salary|paycheck|income) (.*)$)", std::regex::icase),
+        { std::regex(R"(\b(salary|paycheck|income)\b.*$)", std::regex::icase),
           { "How do you feel about %R%?" } },
 
         { std::regex(R"(\bi (lost|lost my) job\b)", std::regex::icase),
